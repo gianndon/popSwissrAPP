a40306d07f65ac52496fa3f222462ab31bb549c8
michilaedi <michilaedi@me.com>
2023-03-24 11:39:18 +0100

    NEWWWWW

--- app.R
+++ app.R
@@ -2,7 +2,27 @@ library(shiny)
 library(popSwissr)
 
 ui <- function(request) {
+  
   fluidPage(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
+    
+    #Navbar
+    navbarPage("Swisscon Portfolio", id ="navbar",
+               tabPanel("Übersicht"                      ),
+               tabPanel("Investment", 
+                        tabsetPanel(
+                          tabPanel("Rendite Maximieren / Risiko Minimieren"),
+                          tabPanel("Minimum Varianz Portfolio", id="navbar"),
+                          tabPanel("Tangentialportfolio"),
+                          tabPanel("Kennzahlen")
+                        )),
+               tabPanel("Über uns", 
+                        tabsetPanel(
+                          tabPanel("Geschichte"),
+                          tabPanel("Kontakt")
+                        ))
+    ),
     plotOutput("plot"),
     sliderInput("n", "Number of observations", 1, nrow(faithful), 100),
     bookmarkButton()
